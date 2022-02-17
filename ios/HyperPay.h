82adf403ce2c2b011fb8954cdc258c985af16db2
RMabroukS <r.mabrouk.sayed@gmail.com>
2022-02-17 05:04:46 +0200

    add createTransaction method

--- ios/HyperPay.h
+++ ios/HyperPay.h
@@ -1,5 +1,11 @@
+#ifndef Hyperpay_h
+#define Hyperpay_h
+#import <OPPWAMobile/OPPWAMobile.h>
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
-@interface HyperPay : NSObject <RCTBridgeModule>
+@interface HyperPay : RCTEventEmitter <RCTBridgeModule>
 
 @end
+
+#endif /* Hyperpay */
--- ios/HyperPay.m
+++ ios/HyperPay.m
@@ -1,11 +1,85 @@
+
+#import <Foundation/Foundation.h>
 #import "HyperPay.h"
+#import <React/RCTLog.h>
 
 @implementation HyperPay
 
-RCT_EXPORT_MODULE()
+OPPPaymentProvider *provider;
 
-// Example method
-// See // https://reactnative.dev/docs/native-modules-ios
+
+
+
+RCT_EXPORT_MODULE(HyperPay)
+
+-(instancetype)init
+{
+    self = [super init];
+    if (self) {
+      #ifdef DEBUG
+        provider = [OPPPaymentProvider paymentProviderWithMode:OPPProviderModeTest];
+     #else
+        provider = [OPPPaymentProvider paymentProviderWithMode:OPPProviderModeLive];
+     #endif
+    }
+    
+    return self;
+}
+
+- (NSArray<NSString *> *)supportedEvents {
+    return @[@"onTransactionComplete"];
+}
+
+/**
+ React Native functions
+ */
+
+RCT_EXPORT_METHOD(createTransaction: (NSDictionary*)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+  NSError * _Nullable error;
+
+    OPPCardPaymentParams *params = [OPPCardPaymentParams cardPaymentParamsWithCheckoutID:[options valueForKey:@"checkoutID"]
+                                                                        paymentBrand:[options valueForKey:@"paymentBrand"]
+                                                                              holder:[options valueForKey:@"holderName"]
+                                                                              number:[options valueForKey:@"cardNumber"]
+                                                                         expiryMonth:[options valueForKey:@"expiryMonth"]
+                                                                          expiryYear:[options valueForKey:@"expiryYear"]
+                                                                                 CVV:[options valueForKey:@"cvv"]
+                                                                               error:&error];
+ 
+    if (error) {
+      NSLog(@"%s", "error");
+      reject(@"createTransaction",error.localizedDescription, error);
+
+    } else {
+       params.shopperResultURL =[options valueForKey:@"shopperResultURL"];
+       
+      OPPTransaction *transaction = [OPPTransaction transactionWithPaymentParams:params];
+
+      [provider submitTransaction:transaction completionHandler:^(OPPTransaction * _Nonnull transaction, NSError * _Nullable error) {
+        NSDictionary *transactionResult;
+        if (transaction.type == OPPTransactionTypeAsynchronous) {
+            
+           transactionResult = @{
+          @"redirectURL":transaction.redirectURL.absoluteString,
+          @"status":@"pending",
+          @"checkoutId":transaction.paymentParams.checkoutID
+          };
+          resolve(transactionResult);
+
+        }  else if (transaction.type == OPPTransactionTypeSynchronous) {
+
+          transactionResult = @{
+          @"status":@"completed",
+          @"checkoutId":transaction.paymentParams.checkoutID
+          };
+          resolve(transactionResult);
+        } else {
+          reject(@"createTransaction",error.localizedDescription, error);
+        }
+      }];
+    }
+}
+
 RCT_REMAP_METHOD(multiply,
                  multiplyWithA:(nonnull NSNumber*)a withB:(nonnull NSNumber*)b
                  withResolver:(RCTPromiseResolveBlock)resolve
@@ -17,3 +91,5 @@ RCT_REMAP_METHOD(multiply,
 }
 
 @end
+
+
