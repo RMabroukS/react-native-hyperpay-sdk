b21c6144fe1a83af3531112d31acc8ba0ed8724f
RMabroukS <r.mabrouk.sayed@gmail.com>
2023-01-22 18:01:36 +0200

    upgrade android & ios sdk to 4.9.0

---
+++ lib/typescript/BankAccountBrands.ts
@@ -0,0 +1,15 @@
+// Bank Account Brands
+export type BankAccountBrands =
+  | 'BITCOIN' // BITCOIN	async
+  | 'BOLETO' // BOLETO	sync
+  | 'DIRECTDEBIT_SEPA' // DIRECTDEBIT_SEPA	sync
+  | 'GIROPAY' // GIROPAY	async
+  | 'IDEAL' // IDEAL	async
+  | 'INTERAC_ONLINE' // INTERAC_ONLINE	async
+  | 'OXXO' // OXXO	async
+  | 'POLI' // POLI	async
+  | 'PREPAYMENT' // PREPAYMENT	async
+  | 'SOFORTUEBERWEISUNG' // SOFORTUEBERWEISUNG	async
+  | 'TRUSTPAY_VA'; // TRUSTPAY_VA	async
+
+
